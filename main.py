67448b215b499ab662e902dbb2d325907e939d7c
arvindnair1995 <rvghostrider@gmail.com>
2024-07-20 18:13:53 +0530

    chore: added pysondb support, strategy pattern for db

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 from typing import Union
-
 from fastapi import FastAPI
+from strategy.db_strategy import PysonDBStrategy
 
 app = FastAPI()
+
+db_strategy = PysonDBStrategy()
+db_strategy.setup()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    return {"message": "Welcome to Atlys backend challenge!"}
